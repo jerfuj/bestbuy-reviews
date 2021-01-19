92a81c040424d02566eb650dd5e592edb59537e1
seanhchen98 <seanhchen98@gmail.com>
2021-01-19 09:42:20 -0800

    working get request, wrote out put requests that still need work. will get back to once I start working on the ajax calls clientside.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,49 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const { getAll } = require('../database/index.js');
+const { updateReview } = require('../database/index.js');
+
+app.get('/api/reviews/:id', (req, res) => {
+  const { id } = req.params;
+  getAll(id, (err, results) => {
+    if (err) {
+      res.status(400).end();
+    }
+    res.status(200).send(results);
+  });
+});
+
+app.put('/api/reviews/:id/helpful', (req, res) => {
+  const { id } = req.params;
+  const value = 'helpful';
+  updateReview(value, id, (err, results) => {
+    if (err) {
+      res.status(400).end();
+    }
+    res.status(200).send(results);
+  });
+});
+
+app.put('/api/reviews/:id/unhelpful', (req, res) => {
+  const { id } = req.params;
+  const { review } = req.params;
+  console.log(id);
+  console.log('review:', review);
+  const value = 'unhelpful';
+  updateReview(value, id, review, (err, results) => {
+    if (err) {
+      res.status(400).end();
+    }
+    res.status(200).send(results);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
